06a0a30133bc6d23daf46fb6c20ef81f4ee0d407
Brian Savidge <bsav0016@gmail.com>
2022-08-21 22:17:27 -1000

    with allowed host heroku

--- newAcftScoreCalculator/settings.py
+++ newAcftScoreCalculator/settings.py
@@ -17,7 +17,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG')
 
-ALLOWED_HOSTS = ['192.168.1.230', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['updated-acft-score-calculator.herokuapp.com']
 
 # Application definition
 
